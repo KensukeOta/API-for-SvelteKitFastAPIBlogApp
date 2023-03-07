c23119d21bf81a1cf453c8318f2f67a2e0c43c55
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-03-07 23:05:46 +0900

    ユーザー登録機能のためのAPIを実装 (#1)

--- models/user.py
+++ models/user.py
@@ -1,15 +1,14 @@
 from typing import Union
-from datetime import datetime
 
 from fastapi import UploadFile
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 
 
 class UserCreate(BaseModel):
-    name: str
+    name: str = Field(min_length=1, max_length=10)
     email: EmailStr
-    password: str
-    password_confirmation: str
+    password: str = Field(min_length=3)
+    password_confirmation: str = Field(min_length=3)
     avatar: Union[UploadFile, None] = None
-    created_at: datetime
-    updated_at: datetime
+    created_at: str
+    updated_at: str
--- routers/users.py
+++ routers/users.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from deta import Deta
+
 from passlib.context import CryptContext
 
+import settings
 from models.user import UserCreate
+
+deta = Deta(settings.PROJECT_KEY)
+
+users = deta.Base("users")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -18,6 +25,17 @@ async def read_user():
 
 @router.post("/users/create")
 async def create_user(user: UserCreate):
-    user.password = pwd_context.hash(user.password)
-    user.password_confirmation = pwd_context.hash(user.password_confirmation)
-    return user
+    if user.password != user.password_confirmation:
+        raise HTTPException(400, detail="Password and Confirm Password values do not match")
+    
+    create_user = users.put({
+        "name": user.name,
+        "email": user.email,
+        "password": pwd_context.hash(user.password),
+        "password_confirmation": pwd_context.hash(user.password_confirmation),
+        "avatar": user.avatar,
+        "created_at": user.created_at,
+        "updated_at": user.updated_at
+    })
+
+    return create_user
