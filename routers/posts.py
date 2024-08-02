5a2b2af58d14286f5945aad813d31c43844d13e6
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-08-02 10:19:04 +0900

    該当の記事１件を返すAPIを実装 (#5)

--- routers/posts.py
+++ routers/posts.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import desc, select, Session
 
 from ..database import get_session
@@ -26,3 +26,11 @@ def create_post(*, session: Annotated[Session, Depends(get_session)], post: Post
 def read_posts(*, session: Annotated[Session, Depends(get_session)]) -> list[PostPublicWithUser]:
     posts = session.exec(select(Post).order_by(desc(Post.created_at))).all()
     return posts
+
+
+@router.get("/posts/{id}")
+def read_post(*, session: Session = Depends(get_session), id: int) -> PostPublicWithUser:
+    post = session.get(Post, id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
